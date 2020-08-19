3997b75ea87bb37ceb45de29e167938084064bf4
Daniel Kim <daniel.seungchul.kim@gmail.com>
2020-08-18 22:54:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function closeModal() {
+  var modal = document.querySelector(".modal");
+  modal.style.visibility = "hidden";
+}
+
+function openModal() {
+  var modal = document.querySelector(".modal");
+  modal.style.visibility = "visible";
+}
